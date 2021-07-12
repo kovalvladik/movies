7c600a9213aa0d62aea32baf865f2226b2450fc9
Vladislav <kovalvlad9@gmail.com>
2021-07-12 15:41:19 +0300

    update https

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,17 +12,25 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=iron man`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=iron man`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false})) //, loading:false (  падает в цикл)
+            .then(data => this.setState({movies: data.Search, loading: false}))
+            .catch((err)=>{
+                console.error(err)
+                this.setState({loading:false})
+            })
     }
 
     SeachMovies = (str, type = 'all') => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
             type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false})) //, loading:false (  падает в цикл)
+            .then(data => this.setState({movies: data.Search, loading: false}))
+            .catch((err)=>{
+                console.error(err)
+                this.setState({loading:false})
+            })
     }
 
     render() {
